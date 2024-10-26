cecefbd8a0cc61fa7a89a82e28979d44bf494bef
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2024-10-26 21:47:12 +0530

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,34 @@
+function changeBG1(){
+    document.body.style.backgroundColor="crimson";
+    document.getElementById("h1").style.color="crimson";
+   
+}
+function changeBG2(){
+    document.body.style.backgroundColor="chocolate";
+    document.getElementById("h1").style.color="chocolate";
+
+}
+function changeBG3(){
+    document.body.style.backgroundColor="cornflowerblue";
+    document.getElementById("h1").style.color="cornflowerblue";
+
+}
+function changeBG4(){
+    document.body.style.backgroundColor="darkolivegreen";
+    document.getElementById("h1").style.color="darkolivegreen";
+
+}
+function changeBG5(){
+    document.body.style.backgroundColor="deeppink";
+    document.getElementById("h1").style.color="deeppink";
+
+}
+function changeBG6(){
+    document.body.style.backgroundColor="greenyellow";
+    document.getElementById("h1").style.color="greenyellow";
+
+}
+function remBG(){
+    document.body.style.backgroundColor="white";
+    document.getElementById("h1").style.color="black";
+}
